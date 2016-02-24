3ba29cdd00a262c7e221c91e73593b24aea656ce
JamesDuBois1994 <jamesdubois@oeconnection.com>
2016-02-24 18:43:36 -0500

    Update
    Got rid of drop down menu!

--- SideKick/WpfStyleableWindow/StyleableWindow/SystemMenuManager.cs
+++ SideKick/WpfStyleableWindow/StyleableWindow/SystemMenuManager.cs
@@ -13,6 +13,7 @@ namespace WpfStyleableWindow.StyleableWindow
     {        
         public static void ShowMenu(Window targetWindow, Point menuLocation)
         {
+            var test = " ";
             if (targetWindow == null)
                 throw new ArgumentNullException("TargetWindow is null.");
 
@@ -29,33 +30,33 @@ namespace WpfStyleableWindow.StyleableWindow
                 y = 0;
             }
 
-            uint WM_SYSCOMMAND = 0x112, TPM_LEFTALIGN = 0x0000, TPM_RETURNCMD = 0x0100;           
+            //uint WM_SYSCOMMAND = 0x112, TPM_LEFTALIGN = 0x0000, TPM_RETURNCMD = 0x0100;           
 
-            IntPtr window = new WindowInteropHelper(targetWindow).Handle;
+            //IntPtr window = new WindowInteropHelper(targetWindow).Handle;
 
-            IntPtr wMenu = NativeMethods.GetSystemMenu(window, false);
+            //IntPtr wMenu = NativeMethods.GetSystemMenu(window, false);
 
-            int command = NativeMethods.TrackPopupMenuEx(wMenu, TPM_LEFTALIGN | TPM_RETURNCMD, x, y, window, IntPtr.Zero);
+            //int command = NativeMethods.TrackPopupMenuEx(wMenu, TPM_LEFTALIGN | TPM_RETURNCMD, x, y, window, IntPtr.Zero);
 
-            if (command == 0)
-                return;
+            //if (command == 0)
+            //    return;
 
-            NativeMethods.PostMessage(window, WM_SYSCOMMAND, new IntPtr(command), IntPtr.Zero);
+            //NativeMethods.PostMessage(window, WM_SYSCOMMAND, new IntPtr(command), IntPtr.Zero);
         }
     }
 
-    internal static class NativeMethods
-    {
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        internal static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+    //internal static class NativeMethods
+    //{
+    //    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    //    internal static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
 
-        [DllImport("user32.dll")]
-        internal static extern int TrackPopupMenuEx(IntPtr hmenu, uint fuFlags, int x, int y, IntPtr hwnd, IntPtr lptpm);
+    //    [DllImport("user32.dll")]
+    //    internal static extern int TrackPopupMenuEx(IntPtr hmenu, uint fuFlags, int x, int y, IntPtr hwnd, IntPtr lptpm);
 
-        [DllImport("user32.dll")]
-        internal static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+    //    [DllImport("user32.dll")]
+    //    internal static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
-        [DllImport("user32.dll")]
-        internal static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
-    }
+    //    [DllImport("user32.dll")]
+    //    internal static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
+    //}
 }
